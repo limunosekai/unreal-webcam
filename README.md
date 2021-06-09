df2bc1092bac107b39eb93b42932469b13c0d3a5
limunosekai <powerlsh0103@gmail.com>
2021-06-09 18:09:23 +0900

    ADD README

---
+++ README.md
@@ -0,0 +1,29 @@
+## UNREAL-WEBCAM
+
+[Go to WEB](https://limunosekai.github.io/unreal-webcam/)
+
+<br>
+
+### ✨ Main Logic
+
+![1](./img/K-001.png)
+
+![](./img/K-002.png)
+
+![](./img/K-003.png)
+
+<br>
+
+<br>
+
+### ✨ Screen Shot
+
+![2](./img/K-004.png)
+
+<br>
+
+<br>
+
+---
+
+Source : [JAVASCRIPT 30](https://javascript30.com/)
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,13 @@ const snap = document.querySelector('.snap');
 const ctx = canvas.getContext('2d');
 
 function getVideo() {
-  navigator.mediaDevices.getUserMedia({ video: true, audio: false})
-    .then(localMediaStream => {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
       video.srcObject = localMediaStream;
       video.play();
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(`OH NO!!`, err);
     });
 }
@@ -23,15 +24,16 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-    let pixels = ctx.getImageData(0,0,width,height);
+    let pixels = ctx.getImageData(0, 0, width, height);
     // pixels = redEffect(pixels);
     pixels = rgbSplit(pixels);
     ctx.globalAlpha = 0.1;
-    ctx.putImageData(pixels,0,0);
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
-function takePhoto() { // button 클릭 시 takaePhoto() 실행
+function takePhoto() {
+  // button 클릭 시 takaePhoto() 실행
   //촬영음 추가
   snap.currentTime = 0;
   snap.play();
@@ -39,30 +41,31 @@ function takePhoto() { // button 클릭 시 takaePhoto() 실행
   // data를 가져와서 canvas에 삽입
   const data = canvas.toDataURL('image/jpeg');
   const link = document.createElement('a'); //a 태그 생성
-  link.href = data;                         //주소는 data
-  link.setAttribute('download', 'sexy');    //속성 부여, 다운로드, 파일명
-  link.innerHTML = `<img src="${data}" alt="sex man"/>` //a링크에 들어갈 img 태그
-  strip.insertBefore(link, strip.firstChild);           //strip의 자식노드인 삽입 하려는 자식노드 link를 strip.firstChild 기준에 맞춰 삽입
+  link.href = data; //주소는 data
+  link.setAttribute('download', 'sexy'); //속성 부여, 다운로드, 파일명
+  link.innerHTML = `<img src="${data}" alt="sex man"/>`; //a링크에 들어갈 img 태그
+  //strip의 자식노드인 삽입 하려는 자식노드 link를 strip.firstChild 기준에 맞춰 삽입
+  strip.insertBefore(link, strip.firstChild);
 }
 
 // 빨간 필터 효과
 function redEffect(pixels) {
-  for(let i = 0; i < pixels.data.length; i += 4) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
     pixels.data[i] = pixels.data[i] + 100;
     pixels.data[i + 1] = pixels.data[i + 1] - 50;
     pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
   }
-  return pixels
+  return pixels;
 }
 
 // RGB 화면 번짐 효과
 function rgbSplit(pixels) {
-  for(let i = 0; i < pixels.data.length; i += 4) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
     pixels.data[i - 350] = pixels.data[i] + 100;
     pixels.data[i + 120] = pixels.data[i + 1] - 50;
     pixels.data[i - 150] = pixels.data[i + 2] * 0.5;
   }
-  return pixels
+  return pixels;
 }
 
 getVideo();
